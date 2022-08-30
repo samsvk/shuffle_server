f63f51773a612bb84045de4cba7391d48e16522f
samsvk <samsvk00@gmail.com>
2022-08-30 15:09:02 +0100

    working random email generator

--- src/routes/controllers.js
+++ src/routes/controllers.js
@@ -1,14 +1,17 @@
 import { email } from "../models/db.js";
+import { randomEmailGenerator } from "../utils.js";
 
 const createNewEmail = async (req, res) => {
   const newEmailObj = req.body;
+  const redirectChildEmail = randomEmailGenerator();
   const emaildb = new email({
     ...newEmailObj,
+    childEmail: [redirectChildEmail],
     createdAt: new Date().toISOString(),
   });
   try {
     await emaildb.save();
-    res.status(201).json(emaildb);
+    res.status(201).json(redirectChildEmail);
   } catch (error) {
     res.status(409).json({ message: error.message });
   }
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 export function randomEmailGenerator() {
   const url = "mask.me";
   // important to check if the rng email is already existing elsewhere
-  const rngHash = (Math.random() + 1).toString(36).substring(12);
+  const rngHash = (Math.random() + 1).toString(36).substring(7);
   return `${rngHash}@${url}`;
 }
